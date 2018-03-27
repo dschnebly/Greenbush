0a1b1227bb693baef07792f0ffe20793a0c8464c
SchneblyD <david.schnebly@greenbush.org>
2018-03-27 10:12:46 -0500

    broke the build. Trying to repair.

--- GreenBushIEP/Models/GreenbushIEPModel.Context.cs
+++ GreenBushIEP/Models/GreenbushIEPModel.Context.cs
@@ -27,48 +27,49 @@ namespace GreenBushIEP.Models
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<tblOrganizationMapping> tblOrganizationMappings { get; set; }
-        public virtual DbSet<tblRole> tblRoles { get; set; }
-        public virtual DbSet<tblUser> tblUsers { get; set; }
-        public virtual DbSet<tblLanguage> tblLanguages { get; set; }
-        public virtual DbSet<tblStudentRelationship> tblStudentRelationships { get; set; }
-        public virtual DbSet<tblStudentInfo> tblStudentInfoes { get; set; }
-        public virtual DbSet<tblIEPAcademic> tblIEPAcademics { get; set; }
-        public virtual DbSet<tblIEPHealth> tblIEPHealths { get; set; }
-        public virtual DbSet<tblIEPIntelligence> tblIEPIntelligences { get; set; }
-        public virtual DbSet<tblIEPMath> tblIEPMaths { get; set; }
-        public virtual DbSet<tblIEPReading> tblIEPReadings { get; set; }
-        public virtual DbSet<tblIEP> tblIEPs { get; set; }
-        public virtual DbSet<tblIEPSocial> tblIEPSocials { get; set; }
-        public virtual DbSet<tblIEPWritten> tblIEPWrittens { get; set; }
-        public virtual DbSet<tblIEPMotor> tblIEPMotors { get; set; }
-        public virtual DbSet<tblIEPCommunication> tblIEPCommunications { get; set; }
-        public virtual DbSet<tblGoalBenchmark> tblGoalBenchmarks { get; set; }
-        public virtual DbSet<tblGoal> tblGoals { get; set; }
-        public virtual DbSet<tblCalendarTemplate> tblCalendarTemplates { get; set; }
-        public virtual DbSet<tblCalendar> tblCalendars { get; set; }
-        public virtual DbSet<tblProvider> tblProviders { get; set; }
-        public virtual DbSet<tblLocation> tblLocations { get; set; }
-        public virtual DbSet<tblService> tblServices { get; set; }
-        public virtual DbSet<tblServiceType> tblServiceTypes { get; set; }
-        public virtual DbSet<tblBuildingMapping> tblBuildingMappings { get; set; }
-        public virtual DbSet<tblBuilding> tblBuildings { get; set; }
-        public virtual DbSet<tblDistrict> tblDistricts { get; set; }
-        public virtual DbSet<tblCalendarReporting> tblCalendarReportings { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<tblAccommodation> tblAccommodations { get; set; }
         public virtual DbSet<tblBehaviorBaseline> tblBehaviorBaselines { get; set; }
         public virtual DbSet<tblBehaviorHypothesi> tblBehaviorHypothesis { get; set; }
         public virtual DbSet<tblBehaviorHypothesisType> tblBehaviorHypothesisTypes { get; set; }
+        public virtual DbSet<tblBehavior> tblBehaviors { get; set; }
         public virtual DbSet<tblBehaviorStrategy> tblBehaviorStrategies { get; set; }
         public virtual DbSet<tblBehaviorStrategyType> tblBehaviorStrategyTypes { get; set; }
         public virtual DbSet<tblBehaviorTrigger> tblBehaviorTriggers { get; set; }
         public virtual DbSet<tblBehaviorTriggerType> tblBehaviorTriggerTypes { get; set; }
+        public virtual DbSet<tblBuildingMapping> tblBuildingMappings { get; set; }
+        public virtual DbSet<tblBuilding> tblBuildings { get; set; }
+        public virtual DbSet<tblCalendar> tblCalendars { get; set; }
+        public virtual DbSet<tblCalendarReporting> tblCalendarReportings { get; set; }
+        public virtual DbSet<tblCalendarTemplate> tblCalendarTemplates { get; set; }
+        public virtual DbSet<tblDistrict> tblDistricts { get; set; }
+        public virtual DbSet<tblGoalBenchmark> tblGoalBenchmarks { get; set; }
+        public virtual DbSet<tblGoal> tblGoals { get; set; }
+        public virtual DbSet<tblIEPAcademic> tblIEPAcademics { get; set; }
+        public virtual DbSet<tblIEPCommunication> tblIEPCommunications { get; set; }
+        public virtual DbSet<tblIEPHealth> tblIEPHealths { get; set; }
+        public virtual DbSet<tblIEPIntelligence> tblIEPIntelligences { get; set; }
+        public virtual DbSet<tblIEPMath> tblIEPMaths { get; set; }
+        public virtual DbSet<tblIEPMotor> tblIEPMotors { get; set; }
+        public virtual DbSet<tblIEPReading> tblIEPReadings { get; set; }
+        public virtual DbSet<tblIEP> tblIEPs { get; set; }
+        public virtual DbSet<tblIEPSocial> tblIEPSocials { get; set; }
+        public virtual DbSet<tblIEPWritten> tblIEPWrittens { get; set; }
+        public virtual DbSet<tblLanguage> tblLanguages { get; set; }
+        public virtual DbSet<tblLocation> tblLocations { get; set; }
+        public virtual DbSet<tblOrganizationMapping> tblOrganizationMappings { get; set; }
+        public virtual DbSet<tblOtherConsideration> tblOtherConsiderations { get; set; }
+        public virtual DbSet<tblProvider> tblProviders { get; set; }
+        public virtual DbSet<tblRole> tblRoles { get; set; }
+        public virtual DbSet<tblService> tblServices { get; set; }
+        public virtual DbSet<tblServiceType> tblServiceTypes { get; set; }
+        public virtual DbSet<tblStudentInfo> tblStudentInfoes { get; set; }
+        public virtual DbSet<tblStudentRelationship> tblStudentRelationships { get; set; }
+        public virtual DbSet<tblTransitionAssessment> tblTransitionAssessments { get; set; }
         public virtual DbSet<tblTransitionGoal> tblTransitionGoals { get; set; }
         public virtual DbSet<tblTransition> tblTransitions { get; set; }
-        public virtual DbSet<tblTransitionAssessment> tblTransitionAssessments { get; set; }
-        public virtual DbSet<tblBehavior> tblBehaviors { get; set; }
         public virtual DbSet<tblTransitionService> tblTransitionServices { get; set; }
-        public virtual DbSet<tblOtherConsideration> tblOtherConsiderations { get; set; }
+        public virtual DbSet<tblUser> tblUsers { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
