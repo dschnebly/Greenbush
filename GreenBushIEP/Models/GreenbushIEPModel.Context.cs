2eaf5958f9c756f7c24a72cdfb318be5c85642ce
SchneblyD <david.schnebly@greenbush.org>
2018-06-04 10:41:28 -0500

    Changed the database.

--- GreenBushIEP/Models/GreenbushIEPModel.Context.cs
+++ GreenBushIEP/Models/GreenbushIEPModel.Context.cs
@@ -27,7 +27,6 @@ namespace GreenBushIEP.Models
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<tblAccommodation> tblAccommodations { get; set; }
         public virtual DbSet<tblBehaviorBaseline> tblBehaviorBaselines { get; set; }
         public virtual DbSet<tblBehaviorHypothesi> tblBehaviorHypothesis { get; set; }
@@ -44,6 +43,7 @@ namespace GreenBushIEP.Models
         public virtual DbSet<tblCalendarTemplate> tblCalendarTemplates { get; set; }
         public virtual DbSet<tblDistrict> tblDistricts { get; set; }
         public virtual DbSet<tblGoalBenchmark> tblGoalBenchmarks { get; set; }
+        public virtual DbSet<tblGoal> tblGoals { get; set; }
         public virtual DbSet<tblIEPAcademic> tblIEPAcademics { get; set; }
         public virtual DbSet<tblIEPCommunication> tblIEPCommunications { get; set; }
         public virtual DbSet<tblIEPHealth> tblIEPHealths { get; set; }
@@ -62,6 +62,7 @@ namespace GreenBushIEP.Models
         public virtual DbSet<tblProvider> tblProviders { get; set; }
         public virtual DbSet<tblRole> tblRoles { get; set; }
         public virtual DbSet<tblServiceFrequency> tblServiceFrequencies { get; set; }
+        public virtual DbSet<tblService> tblServices { get; set; }
         public virtual DbSet<tblServiceType> tblServiceTypes { get; set; }
         public virtual DbSet<tblStudentInfo> tblStudentInfoes { get; set; }
         public virtual DbSet<tblStudentRelationship> tblStudentRelationships { get; set; }
@@ -70,8 +71,6 @@ namespace GreenBushIEP.Models
         public virtual DbSet<tblTransition> tblTransitions { get; set; }
         public virtual DbSet<tblTransitionService> tblTransitionServices { get; set; }
         public virtual DbSet<tblUser> tblUsers { get; set; }
-        public virtual DbSet<tblGoal> tblGoals { get; set; }
-        public virtual DbSet<tblService> tblServices { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
