85b0fe24fbffba4574ae6d747c225e9296f39e1f
SchneblyD <david.schnebly@greenbush.org>
2018-10-03 10:47:22 -0500

    Implement Access Levels

--- GreenBushIEP/Scripts/nurse.js
+++ GreenBushIEP/Scripts/nurse.js
@@ -8,13 +8,6 @@
             $(".ajax-loader img").show();
         });
 
-        // attach Event
-        // fires when a user clicks on the edit user button
-        $("#editStudent").on("click", function () {
-            $(".ajax-loader").show();
-            $(".ajax-loader img").show();
-        });
-
         // attach event
         // finds all launchIEP on the page and fires when the link is clicked.
         $('.launchIEP').each(function (index) {
@@ -23,40 +16,6 @@
                 $(".ajax-loader").show();
                 $(".ajax-loader img").show();
             });
-        });
-
-        // attach event
-        // fires when an delete button is pressed.
-        $('#deleteUser').on('show.bs.modal', function (e) {
-            var user = $(e.relatedTarget).data('id');
-            $(e.currentTarget).find('input[name="id"]').val(user);
-            $('#confirmDeletion').val('');
-        });
-
-        // attach event
-        // fires when you click yes on the module deleteForm.
-        $('#deleteUser button[type=submit]').on('click', function (e) {
-            if ($('#confirmDeletion').val() === 'DELETE') {
-                var userId = $(e.currentTarget).parent().parent().find('input[name="id"]').val();
-                $.ajax({
-                    type: 'POST',
-                    url: '/Manage/Delete',
-                    data: { id: userId },
-                    dataType: "json",
-                    success: function (data) {
-                        var userRow = $("tr[data-id=" + userId + "]")
-                        userRow.remove();
-
-                        $("#alertMessage .moreinfo").html(data.Message);
-                        $("#alertMessage").fadeTo(2000, 500).slideUp(500, function () {
-                            $("#alertMessage").slideUp(500);
-                        });
-                    },
-                    error: function (data) {
-                        console.log("Unable to connect to the server or another related problem.");
-                    }
-                });
-            }
         });
     }
     init();
