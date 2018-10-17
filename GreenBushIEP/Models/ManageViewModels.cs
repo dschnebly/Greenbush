660bb2a47761c31051858fe2338475005a8ce907
Laurie Long <long.laurie@gmail.com>
2018-10-17 16:16:44 -0500

    Available student pop up has incorrect county and school info (hard coded).

--- GreenBushIEP/Models/ManageViewModels.cs
+++ GreenBushIEP/Models/ManageViewModels.cs
@@ -168,7 +168,8 @@ namespace GreenBushIEP.Models
         public string LastName { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string Email { get; set; }
+		public string County { get; set; }
+		public string Email { get; set; }
         public string Password { get; set; }
         public string RoleID { get; set; }
         public string BuildingID { get; set; }
@@ -188,7 +189,8 @@ namespace GreenBushIEP.Models
         public string Zip { get; set; }
         public string IEPDate { get; set; }
         public bool hasIEP { get; set; }
-    }
+		
+	}
 
     public class StudentViewModel
     {
