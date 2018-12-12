8b0cfdec868af3828b9562fda8ae7a0771a4534e
Laurie Long <long.laurie@gmail.com>
2018-12-12 13:39:43 -0600

    Remove inline CSS and javascript code and place them into individual css files and js files that are loaded dynamically to increase speed on load. - Portal Pages Level 2 (MIS)

---
+++ GreenBushIEP/Scripts/iepforms.js
@@ -0,0 +1,16 @@
+﻿$(".printForm").on("click", function (e) {
+
+	var id = getUrlParameter('stid');
+
+	if (id) {
+		$('.ajax-loader').css("visibility", "visible");
+		var pageName = this.id;
+		window.location.href = '/Home/' + pageName + '/' + id;
+	}
+	else {
+		$("#alertMessage .moreinfo").html('Server Error');
+		$("#alertMessage").fadeTo(3000, 500).slideUp(500, function () {
+			$("#alertMessage").slideUp(500);
+		});
+	}
+});
