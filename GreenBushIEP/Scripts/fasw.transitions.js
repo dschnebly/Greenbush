d6c0773cff668c68caf043177886f814983eadc5
SchneblyD <david.schnebly@greenbush.org>
2019-01-24 15:03:32 -0600

    If you have a list of students in your rooster and you click on the IEP is loads at the bottom of the page.

--- GreenBushIEP/Scripts/fasw.transitions.js
+++ GreenBushIEP/Scripts/fasw.transitions.js
@@ -87,6 +87,7 @@ function ft(params) {
         setTimeout(function () { d2.className = e + " in"; d2.style.display = "block"; }, 1); // so we don't see a flash of the other page upon button click.
         setTimeout(function () {
             document.body.innerHTML = d2.innerHTML;
+            document.body.scrollTop = 0;
 
             cBa();
             Reload();
