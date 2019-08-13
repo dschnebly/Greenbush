0d02325f4b3e014a0a0dd18c3f9e4f4ccf0ca39e
dschnebly <34659588+dschnebly@users.noreply.github.com>
2019-08-13 11:56:06 -0500

    fixed an issue with the calender month display.

--- GreenBushIEP/Properties/AssemblyInfo.cs
+++ GreenBushIEP/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.2.2")]
-[assembly: AssemblyFileVersion("2.2.2.2")]
+[assembly: AssemblyVersion("3.3.3.2")]
+[assembly: AssemblyFileVersion("3.3.3.2")]
 
-[assembly: AssemblyInformationalVersion("2.2.0")]
+[assembly: AssemblyInformationalVersion("3.3.0")]
