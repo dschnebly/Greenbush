eeb44805e3354274f601913b079244ba07888236
Laurie Long <long.laurie@gmail.com>
2019-09-04 12:39:07 -0500

    Delete Button on referrals does not work or some more changes need pushed to make it work?

--- GreenBushIEP/Scripts/managereferrals.js
+++ GreenBushIEP/Scripts/managereferrals.js
@@ -6,37 +6,37 @@
 
 function init() {
 	
-	$(".delReferral").on("click", function () {
-		var answer = confirm("Are you sure you want to delete this Referral?");
-		if (answer) {
-			var row = $(this).closest('tr');
-			var referralId = $(this).attr('data-val');
-			var args = { referralId: referralId };
-			$.ajax({
-				url: '/Manage/DeleteReferral',
-				type: 'POST',
-				data: args,
-				success: function (data) {
-					if (data.Result === "success") {
-						row.hide();
-					} else {
-						alert(data.Message);
-					}
-				},
-				error: function (data) {
-					alert("There was an error when attempting to connect to the server.");
-				}
-			});
-		}
-
-		return false;
-	});	 
-
 	$("#filterList").on("change", function () {
 		var selectedVal = parseInt($("#filterList").val());
 		getData(selectedVal);
 	});	 
 	
+}
+
+function deleteReferral(rid,element) {
+	var answer = confirm("Are you sure you want to delete this Referral?");
+	if (answer) {
+		var row = $(element).closest('tr');
+		var referralId = rid;
+		var args = { referralId: referralId };
+		$.ajax({
+			url: '/Manage/DeleteReferral',
+			type: 'POST',
+			data: args,
+			success: function (data) {
+				if (data.Result === "success") {
+					row.hide();
+				} else {
+					alert(data.Message);
+				}
+			},
+			error: function (data) {
+				alert("There was an error when attempting to connect to the server.");
+			}
+		});
+	}
+
+	return false;
 }
 
 function getData(searchVal) {
@@ -70,7 +70,7 @@ function getData(searchVal) {
 					content += '<td  style="vertical-align:top">';
 					content += '<a title="View" class="btn btn-info " role="button" href="/Manage/EditReferral/' + data.FilterList[m].referralId +'" data-toggle="tooltip" "><span class="glyphicon glyphicon-pencil"></span></a>';
 					content += '</td>';
-					content += '<td style="vertical-align:top;"><a title="Delete" class="btn btn-info delReferral" role="button" data-val="' + data.FilterList[m].referralId +'" style="border: none!important;" data-toggle="tooltip" ><span class="glyphicon glyphicon-trash"></span></a>';
+					content += '<td style="vertical-align:top;"><a title="Delete" class="btn btn-info delReferral" role="button" style="border: none!important;" data-toggle="tooltip" onclick="deleteReferral('+ data.FilterList[m].referralId +', this);"><span class="glyphicon glyphicon-trash"></span></a>';
 					content += '</td>';
 					content += '</tr>';
 				}
