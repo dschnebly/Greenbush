912efc65e2c9da614b6019749ae0babe01679686
dschnebly <34659588+dschnebly@users.noreply.github.com>
2019-09-11 11:56:18 -0500

    Services calendar needs some work! Save Draft, Save and Complete, Duplicate Service and Duplicate All services not working well. Fiscal year and dates are strange.

--- GreenBushIEP/Properties/AssemblyInfo.cs
+++ GreenBushIEP/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("101.101.101.2")]
-[assembly: AssemblyFileVersion("100.100.100.2")]
+[assembly: AssemblyVersion("102.102.102.2")]
+[assembly: AssemblyFileVersion("101.101.101.2")]
 
-[assembly: AssemblyInformationalVersion("101.101.0")]
+[assembly: AssemblyInformationalVersion("102.102.0")]
