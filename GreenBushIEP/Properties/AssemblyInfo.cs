43ef1347959c416b44e6c37ff43fe0f513a08328
dschnebly <34659588+dschnebly@users.noreply.github.com>
2019-09-12 12:30:18 -0500

    fixed a bug

--- GreenBushIEP/Properties/AssemblyInfo.cs
+++ GreenBushIEP/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("104.104.104.2")]
-[assembly: AssemblyFileVersion("103.103.103.2")]
+[assembly: AssemblyVersion("105.105.105.2")]
+[assembly: AssemblyFileVersion("104.104.104.2")]
 
-[assembly: AssemblyInformationalVersion("104.104.0")]
+[assembly: AssemblyInformationalVersion("105.105.0")]
