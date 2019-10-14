0ce431b6620e08f071561f3761ecd1dfa93b5be6
dschnebly <34659588+dschnebly@users.noreply.github.com>
2019-10-14 10:56:42 -0500

    Revert "Print if the IEP is an annual or amendment on Print IEP"
    This reverts commit 483f458359c407701d1c333e9d89a7379cd6696d.

--- GreenBushIEP/Controllers/HomeController.cs
+++ GreenBushIEP/Controllers/HomeController.cs
@@ -2315,16 +2315,11 @@ namespace GreenbushIep.Controllers
 
                 };
 
+
+                //student goalds
                 if (theIEP != null && theIEP.current != null)
                 {
-					//status of iep
-					var listOfStudentsIEPs = db.tblIEPs.Where(i => i.UserID == stid && i.IsActive && i.IepStatus != IEPStatus.ARCHIVE).OrderBy(i => i.IepStatus).ThenBy(i => i.Amendment).ToList();
-					theIEP.anyStudentIEPActive = listOfStudentsIEPs.Any(i => i.IepStatus.ToUpper() == IEPStatus.ACTIVE && i.IsActive); ;
-					theIEP.anyStudentIEPAmendment = listOfStudentsIEPs.Any(i => i.IepStatus.ToUpper() == IEPStatus.DRAFT && i.Amendment && i.IsActive);
-					theIEP.anyStudentIEPDraft = listOfStudentsIEPs.Any(i => i.IepStatus.ToUpper() == IEPStatus.DRAFT && !i.Amendment && i.IsActive); ;
-					theIEP.anyStudentIEPAnnual = listOfStudentsIEPs.Any(i => i.IepStatus.ToUpper() == IEPStatus.ANNUAL && i.IsActive);
-					
-					theIEP.studentGoals = db.tblGoals.Where(g => g.IEPid == theIEP.current.IEPid).ToList();
+                    theIEP.studentGoals = db.tblGoals.Where(g => g.IEPid == theIEP.current.IEPid).ToList();
                     foreach (var goal in theIEP.studentGoals)
                     {
                         theIEP.studentGoalBenchmarks.AddRange(db.tblGoalBenchmarks.Where(g => g.goalID == goal.goalID).ToList());
@@ -2419,8 +2414,7 @@ namespace GreenbushIep.Controllers
 
                     theIEP.studentDetails = studentDetails;
 
-					
-					return theIEP;
+                    return theIEP;
                 }
             }
 
