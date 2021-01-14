b5e182790fc1bf5e9d82b014a52fd7fbb83dc11f
Laurie Long <long.laurie@gmail.com>
2021-01-14 11:25:06 -0600

    Prior Written Notice -ID - fix line breaks in the multi primary contact print

--- GreenBushIEP/Controllers/HomeController.cs
+++ GreenBushIEP/Controllers/HomeController.cs
@@ -4419,7 +4419,6 @@ namespace GreenbushIep.Controllers
                 {
                     result = System.Text.RegularExpressions.Regex.Replace(HTMLContent, @"\r\n?|\n", "");
                     result = System.Text.RegularExpressions.Regex.Replace(result, @"new-line-val", "<br/>");
-
                 }
 
                 string cssTextResult = System.Text.RegularExpressions.Regex.Replace(cssText, @"\r\n?|\n", "");
@@ -4428,7 +4427,7 @@ namespace GreenbushIep.Controllers
                 if (!string.IsNullOrEmpty(StudentHTMLContent))
                 {
                     string result2 = System.Text.RegularExpressions.Regex.Replace(StudentHTMLContent, @"\r\n?|\n", "");
-                    //result2 = System.Text.RegularExpressions.Regex.Replace(result2, @"textarea", "p");
+                    result2 = System.Text.RegularExpressions.Regex.Replace(StudentHTMLContent, @"new-line-val", "<br/>");                    
                     studentFile = CreatePDFBytes(cssTextResult, result2, "studentInformationPage", imgfoot, "", isDraft, false);
                 }
 
@@ -4436,7 +4435,7 @@ namespace GreenbushIep.Controllers
                 if (!string.IsNullOrEmpty(HTMLContent2))
                 {
                     string secondaryPage = System.Text.RegularExpressions.Regex.Replace(HTMLContent2, @"\r\n?|\n", "");
-                    //secondaryPage = System.Text.RegularExpressions.Regex.Replace(secondaryPage, @"</?textarea>", "");
+                    secondaryPage = System.Text.RegularExpressions.Regex.Replace(HTMLContent2, @"new-line-val", "<br/>");                    
                     secondaryPageFile = CreatePDFBytes(cssTextResult, secondaryPage, "module-page", imgfoot, studentName, isDraft, true);
                 }
 
@@ -4444,7 +4443,7 @@ namespace GreenbushIep.Controllers
                 if (!string.IsNullOrEmpty(HTMLContent3))
                 {
                     string thirdPage = System.Text.RegularExpressions.Regex.Replace(HTMLContent3, @"\r\n?|\n", "");
-                    //thirdPage = System.Text.RegularExpressions.Regex.Replace(thirdPage, @"</?textarea>", "");
+                    thirdPage = System.Text.RegularExpressions.Regex.Replace(HTMLContent3, @"new-line-val", "<br/>");                    
                     thirdPageFile = CreatePDFBytes(cssTextResult, thirdPage, "module-page", imgfoot, studentName, isDraft, true);
                 }
 
